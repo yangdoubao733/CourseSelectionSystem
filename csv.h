6ad85458058cc38c0b97bd654be50e021463e381
yangdoubao733 <yang1243111380@126.com>
2024-11-21 18:48:01 +0800

    Update csv.h

--- csv.h
+++ csv.h
@@ -1,7 +1,28 @@
 #ifndef CSV
 #define CSV
 
-void read();
-void write();
+struct course
+{
+    int id;
+    char name;
+    char property;
+    int credit;
+    int classHour;
+    int practiceHour;
+    int term;
+};
+struct student
+{
+    char name;
+    int id;
+    int selectedCourseId;
+};
+
+
+
+struct course readCourseInfo(); //返回一个数组，包含所有课程
+int writeCourseInfo(); //输入一个数组，将课程写入文件course.csv
+struct student readStudentInfo(); //返回一个数组包含所有学生
+int writeStudentInfo(); //将学生信息写入文件student.csv
 
 #endif

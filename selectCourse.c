295769ff11beecd8002fa091f9fe66691ba7345f
yigehuaidan <24211126@bjtu.edu.cn>
2024-11-30 22:11:50 +0800

    Update selectCourse.c

--- selectCourse.c
+++ selectCourse.c
@@ -1,3 +1,21 @@
+//张轶淮
+#include<stdio.h>
+#include"csv.h"
+#include"menu.h"
+
 void selectCourse(){
-    
+    printf("请输入您的姓名学号");
+    struct student studentInfo;
+    int n;
+    scanf("%s %d",&studentInfo.student_name,&studentInfo.student_id);
+    printf("请依次输入您要选的课程id，输入‘-1’表示结束(注；每人仅可选择一次，结束后不可修改)");
+    for (size_t i = 0; i <=MAX_VALUE; i++)
+    {scanf("%d\n",&n);
+    n=studentInfo.student_selectedCourseId[i];
+     if (n==-1)
+     {printf("选课结束");
+    n=studentInfo.student_selectedCourseId[i];
+     break;
+     }
+     }
 }
